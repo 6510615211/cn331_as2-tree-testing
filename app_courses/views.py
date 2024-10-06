f7a339bffcac1c8dc3afbc10662185ec59e9bc56
6510685057 <110814202+6510685057@users.noreply.github.com>
2024-10-07 01:03:19 +0700

    update

--- app_courses/views.py
+++ app_courses/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from app_courses.models import Subject
 # Create your views here.
 
 
 def courses(request):
-    return render(request, 'app_courses/courses.html')
+    subjects = Subject.objects.all()
+    return render(request, "app_courses/courses.html", {"subjects":subjects})
