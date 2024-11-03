6ecc8f0eac899a6b3cfa2dd32e072c4e0aa8ce00
6510685057 <110814202+6510685057@users.noreply.github.com>
2024-11-03 17:08:37 +0700

    finish test as4.2

--- app_courses/views.py
+++ app_courses/views.py
@@ -1,41 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
-#from django.http.response import HttpResponse
 from app_courses.models import Subject
 
 # Create your views here.
 
   
 def courses(request):
-    subjects = Subject.objects.all()
-    #if request.method == 'POST':
-     #   subject_id = request.POST.get('subject_id')
-
-        
-        #subject = get_object_or_404(Subject, id=subject_id)
-        #subject.course_status = "REGISTERED"
-        #subject.course_amount -= 1
-        #subject.save()
-
-        #return redirect('courses')    
+    subjects = Subject.objects.all()  
     return render(request, "app_courses/courses.html", {"Subject":subjects})
 
-
-
-'''def enroll_check(request):
-    subjects = Subject.objects.all()
-    if request.method == 'POST':
-        subject_id = request.POST.get('subject_id')
-        subject = get_object_or_404(Subject, id=subject_id)
-        
-        #course_name = request.POST.get('course_name')  
-        #semester = request.POST.get('semester')  
-
-        if subject_id:
-            
-            subject = get_object_or_404(Subject, id=subject_id)
-        return redirect('courses')
-    return render(request,'app_courses/enroll_check.html')'''
-    
+   
 
 def enroll_check(request):
     return render(request,'app_courses/enroll_check.html')
--- app_registation/urls.py
+++ app_registation/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path('', views.home,name='home'),
     path('about', views.about,name='about'),
     path('enroll_check', views.enroll_check,name='enroll_check')
-    
-    #path('courses',views.courses,name='courses'),
-
 ]
